c96ba94ef7677df4eca41e1322997a9a0967236f
Lewischip <lewis.chippendale@hotmail.co.uk>
2024-12-05 11:56:02 +0000

    Launch Library 2 Connection Start

---
+++ RLP_External_Data_Ingest/LaunchDataGet.cs
@@ -0,0 +1,46 @@
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace RLP_External_Data_Ingest;
+
+public class LaunchDataGet
+{
+    private readonly HttpClient client;
+
+    // Constructor for the class where we initialise API connections
+    public LaunchDataGet()
+    {
+        // TODO - Put this in an appsettings
+        client = new HttpClient();
+        client.BaseAddress = new Uri("https://ll.thespacedevs.com/");
+        client.Timeout = new TimeSpan(0, 0, 30);
+        client.DefaultRequestHeaders.Clear();
+        client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+
+    }
+
+    // Gets the launch data from the Launch Library 2 API
+    // See https://ll.thespacedevs.com/docs/#/launches/launches_previous_list for reference
+    public async Task LaunchAPIRetrieval()
+    {
+        HttpResponseMessage response = await client.GetAsync("/2.3.0/launches/previous/?limit=10&offset=10");
+
+        // Checks if a successful response code has been outputted for the connection
+        if (response.IsSuccessStatusCode)
+        {
+            Console.WriteLine("Success");
+
+            // Reads the JSON content of the API response
+            var res = await response.Content.ReadAsStringAsync();
+            var resObj = JsonSerializer.Deserialize<JsonObject>(res);
+
+
+            return;
+        }
+
+        Console.WriteLine("Fail");
+    }
+
+}
--- RLP_External_Data_Ingest/Worker.cs
+++ RLP_External_Data_Ingest/Worker.cs
@@ -2,6 +2,7 @@ namespace RLP_External_Data_Ingest;
 
 public class Worker : BackgroundService
 {
+    // Using ILogger for Console Logging and Debugging
     private readonly ILogger<Worker> _logger;
 
     public Worker(ILogger<Worker> logger)
@@ -11,11 +12,20 @@ public class Worker : BackgroundService
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        _logger.LogInformation("Data Ingest App Starting", DateTimeOffset.Now);
+
+        // Use Ctrl + C to stop the Data Ingest
         while (!stoppingToken.IsCancellationRequested)
         {
             if (_logger.IsEnabled(LogLevel.Information))
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                // TODO - Checks if the worker is still in use
+
+                // TODO - Checks if the data has been updated recently based on appsettings
+                LaunchDataGet dataGet = new();
+                dataGet.LaunchAPIRetrieval();
+
+
             }
             await Task.Delay(1000, stoppingToken);
         }
