7238d41fb5a3ab634e8acb5393e0d600c12284c6
Lewischip <lewis.chippendale@hotmail.co.uk>
2025-03-17 14:23:31 +0000

    Parameter fix for Model Loader

--- RLP_API/ModelLoader.py
+++ RLP_API/ModelLoader.py
@@ -25,7 +25,7 @@ class WeatherParams:
         self.temperature_180m = params["Temperature180m"]
 
 class RocketParams:
-    def _init(self, params):
+    def __init__(self, params):
         self.to_thrust = params["ToThrust"]
         self.launch_mass = params["LaunchMass"]
         self.rocket_length = params["RocketLength"]
@@ -35,8 +35,9 @@ class RocketParams:
 
 # Load the model from the file system
 def load_model(model_type):
-    # TODO - Load the model from the file system
-    model_path = os.path.join(os.path.dirname(__file__), '..', 'RLP_ML_Engine', 'RLP_ML_Models')
+    # Load the model from the file system
+    # model_path = os.path.join(os.path.dirname(__file__), '..', 'RLP_ML_Engine', 'RLP_ML_Models')
+    model_path = os.path.abspath(os.path.join(os.getcwd(), '..', 'RLP_ML_Engine', 'RLP_ML_Models'))
     
     if not os.path.exists(model_path):
         raise FileNotFoundError(f"The directory {model_path} does not exist.")
@@ -63,9 +64,8 @@ if __name__ == "__main__":
 
         # Load selected model
         model = load_model(params["ModelType"])
-        print(model)
 
-        # Create an empty dataframe
+        # # Create an empty dataframe
         input_data = pd.DataFrame()
         
         # Check that the weather parameters are not null
@@ -74,23 +74,53 @@ if __name__ == "__main__":
             # Map the weather params to the model input
             weather_params = WeatherParams(params["WeatherParams"])
 
+            # Add the weather parameters to the input_data dataframe
+            input_data["Rain"] = [weather_params.rain]
+            input_data["Showers"] = [weather_params.showers]
+            input_data["Snowfall"] = [weather_params.snowfall]
+            input_data["CloudCover"] = [weather_params.cloud_cover]
+            input_data["CloudCoverLow"] = [weather_params.cloud_cover_low]
+            input_data["CloudCoverMid"] = [weather_params.cloud_cover_mid]
+            input_data["CloudCoverHigh"] = [weather_params.cloud_cover_high]
+            input_data["Visibility"] = [weather_params.visibility]
+            input_data["Temperature"] = [weather_params.temperature]
+            input_data["WindSpeed10m"] = [weather_params.wind_speed_10m]
+            input_data["WindSpeed80m"] = [weather_params.wind_speed_80m]
+            input_data["WindSpeed120m"] = [weather_params.wind_speed_120m]
+            input_data["WindSpeed180m"] = [weather_params.wind_speed_180m]
+            input_data["Temperature80m"] = [weather_params.temperature_80m]
+            input_data["Temperature120m"] = [weather_params.temperature_120m]
+            input_data["Temperature180m"] = [weather_params.temperature_180m]
+
         # Check that the rocket parameters are not null
         if params["RocketParams"] is not None:
 
             # Map the weather params to the model input
             rocket_params = RocketParams(params["RocketParams"])
 
+            # Add the rocket parameters to the input_data dataframe
+            input_data["ToThrust"] = [rocket_params.to_thrust]
+            input_data["LaunchMass"] = [rocket_params.launch_mass]
+            input_data["RocketLength"] = [rocket_params.rocket_length]
+            input_data["RocketDiameter"] = [rocket_params.rocket_diameter]
+            input_data["SuccessfulRocketLaunches"] = [rocket_params.successful_rocket_launches]
+            input_data["FailedRocketLaunches"] = [rocket_params.failed_rocket_launches]
+
         # Checks if any parameters have been added to the input data
         if input_data.empty:
             raise ValueError("No parameters have been added to the input data.")
+        
+        # Replace any NaN values with 0
+        input_data.fillna(0, inplace=True)
 
         # TODO - Generate Predictions of the model using the input data from RLP_API
-        # pred = model.predict(X_test > 0.5).astype("int32")
+        pred = model.predict(input_data).astype("int32")
+        output = {}
 
         # TODO - Add appropriate attributes to the response object
         # Create response object
         response = {
-            "Prediction": None,
+            "Prediction": output,
             "Status": "Success",
             "Message": "Prediction was successful"
         }
