866bb19508b41ec67998b503bfa0cebab7aaaa53
Lewischip <lewis.chippendale@hotmail.co.uk>
2025-03-17 18:49:17 +0000

    Fixed ModelType output

--- RLP_API/Models/PredictionMakeDto.cs
+++ RLP_API/Models/PredictionMakeDto.cs
@@ -4,7 +4,7 @@ namespace RLP_API.Models
     {
         public WeatherParameters? ParamsWeather { get; set; }
         public RocketParameters? ParamsRocket { get; set; }
-        public ModelType? ModelType { get; set; }
+        public string? ModelType { get; set; }
         public string? PredictedStatus { get; set; }
         public DateTime? CreatedAt { get; set; }
     }
